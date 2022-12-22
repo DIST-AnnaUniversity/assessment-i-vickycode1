c6afe6eec0ed7a538dc788c54e139afacf906473
vickycode1 <116750340+vickycode1@users.noreply.github.com>
2022-12-22 14:15:53 +0530

    Delete range.py

--- range.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv("iris.csv")
-def range():
-    #calculate the range of pw#
-    range_pw = df.pw.max()-df.pw.min()
-    return (range_pw) 
-
-rn = range()
-print(rn)
