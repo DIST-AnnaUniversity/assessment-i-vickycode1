a64f0aa2ea233fb97c62128cc0040c609dd9ba18
vickycode1 <116750340+vickycode1@users.noreply.github.com>
2022-12-22 14:20:52 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -6,23 +6,33 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 
-#read csv file
+df = pd.read_csv("./iris.csv")
 #Take X as petal length
+X = df['pl']
 #Take y as petal width
+y = df['pw']
 
+X = np.array(X).reshape(1, -1).transpose()
 #Split train and test with 30% and set random state as 0
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
 #use regression from scikit learn
 
+
+rg = LinearRegression()
+rg.fit(X_train, y_train)
+
 #find b and round off to 2 decimal places
+coef = round(rg.coef_[0],2)
+print(coef)
 #find a and round off to 2 decimal places
+intercept = round(rg.intercept_, 2)
+print(intercept)
 
 new_pl = 5.3#new x value
 #calculate y 
-new_pw = 
-
+new_pw = intercept + (coef * new_pl)
 print(round(new_pl,2))#round off to 2 decimal places
-
+print(round(new_pw,2))#round off to 2 decimal places
 
 
 
