6966423b73bb1fe37fec088d5af4f8f7a0d8c5cd
vickycode1 <116750340+vickycode1@users.noreply.github.com>
2022-12-22 14:02:46 +0530

    Update range.py

--- range.py
+++ range.py
@@ -1,10 +1,9 @@
 import pandas as pd
-
+df = pd.read_csv("iris.csv")
 def range():
     #calculate the range of pw#
-    range_pw = 
+    range_pw = df.pw.max()-df.pw.min()
     return (range_pw) 
 
 rn = range()
 print(rn)
-
